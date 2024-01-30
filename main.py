d0377eadf1980d82d2db3763005cdad3ba253b06
Sree Duggirala <sreevanthduggirala@gmail.com>
2024-01-29 19:46:39 -0600

    reinstall typer

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import typer
 
+
 app = typer.Typer()
 
 
 # @notice: Fetches RPC URLs for the specified chain
 # @params: Chain name
 @app.command()
-def get_url(name: str):
+def get_url_name(name: str):
     print(name)
 
 
 # @notice: Fetches RPC URLs for the specified chain
 # @params: Chain ID
 @app.command()
-def get_url(id: int):
+def get_url_id(id: int):
     print(id)
 
 
 # @notice: Outputs chain IDs for all EVM-compatible mainnets and testnets
 @app.command()
-def ids():
+def id():
     print("all ids")
 
 
