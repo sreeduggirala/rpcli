a96ba031c7a569a6f38d1a5d938bfd734a72a7f1
adityarai10101 <adyrai05@gmail.com>
2024-01-30 18:55:56 -0600

    BIG DATA

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import typer
 
-
 # @notice: Fetches RPC URLs for the specified chain
 # @params: Chain name
 def url_by_name(name: str):
@@ -16,3 +15,5 @@ def url_by_id(id: int):
 # @notice: Outputs chain IDs for all EVM-compatible mainnets and testnets
 def all_ids():
     print("all ids")
+
+url_by_id(1)
